f4921c868b57bb0c2a2a6853002f6ff0d79e333b
Wes Anderson <wesley.herb.anderson@gmail.com>
2026-03-03 07:45:09 -0800

    feat: add --verbose flag for debug logging (#7)

--- cmd/pb/main.go
+++ cmd/pb/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -9,15 +10,32 @@ import (
 
 var (
 	jsonOutput bool
+	verbose    bool
 )
 
 func newRootCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "pb",
 		Short: "Pebble -- A minimal task tracker for developers who pair with agentic navigators.",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			if verbose || os.Getenv("PB_DEBUG") == "1" {
+				opts := &slog.HandlerOptions{
+					Level: slog.LevelDebug,
+				}
+				var handler slog.Handler
+				if jsonOutput {
+					handler = slog.NewJSONHandler(os.Stderr, opts)
+				} else {
+					handler = slog.NewTextHandler(os.Stderr, opts)
+				}
+				slog.SetDefault(slog.New(handler))
+			}
+			return nil
+		},
 	}
 
 	cmd.PersistentFlags().BoolVar(&jsonOutput, "json", false, "Output in JSON format")
+	cmd.PersistentFlags().BoolVar(&verbose, "verbose", false, "Enable debug logging")
 
 	return cmd
 }
